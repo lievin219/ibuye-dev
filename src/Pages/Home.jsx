f2bc7607d5406a4254a8deec5fe974b4bc832afa
lievin219 <gakizalievin219@gmail.com>
2025-04-12 10:10:22 +0200

    added some home feauture that was left

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import servicess from '/houses.jpg'; // Make sure this path is correct
 
 const Home = () => {
   return (
-    <div>
-      hy rwanda
+    <div
+      className="w-full h-screen bg-cover bg-center flex items-center justify-center justify-start"
+      style={{ backgroundImage: `url(${servicess})` }}
+    >
+      <div className=" bg-opacity-60 p-8 rounded-lg text-white  text-left flex flex-col">
+        <h1 className="text-4xl font-bold mb-4">Invest in your </h1>
+       <h1 className='text-4xl font-bold mb-4'>Future property</h1>
+       <p className=' text-sm max-w-lg'>Ibuye represents a fresh perspective on living, acquiring
+        , and possessing real estate, including opportunities for leasing.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
