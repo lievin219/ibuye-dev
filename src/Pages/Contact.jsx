97ee8a3b69ffb2a83ec9f9aebc840bcc05253d52
lievin219 <gakizalievin219@gmail.com>
2025-04-14 14:10:30 +0200

    Home feauture

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='flex flex-row pt-20'>
-      <div className=''>
-    <h1>Contact us!</h1>
+      <div
+  style={{ backgroundColor: '#004e92' }}
+  className="text-white p-4 rounded-lg max-w-md text-sm leading-relaxed shadow-md ml-20 h-80 ml-1"
+>
+<h1 className='pt-16'>Contact us!</h1>
       </div>
       <div>
 
